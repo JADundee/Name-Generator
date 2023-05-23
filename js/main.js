702bcc06ad74d4a29a9a37b3f3eda1106c2daa01
JADundee <Jadundee@icloud.com>
2023-05-23 07:03:13 -0600

    Added clearSuggestions function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 import { nameOne, nameTwo } from "./names.js";
+
+const initApp = () => {
+    document.getElementById("submitForm").addEventListener("submit", (event) => {
+        event.preventDefault();
+        clearSuggestions();
+        // Generate names
+        // Display names
+    });
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const clearSuggestions = () => {
+    const display = document.getElementById("suggestionSection");
+    if (!display.classList.contains("hidden")) display.classList.toggle("hidden");
+    const list = document.querySelector('.suggestionSection ol');
+    list.innerHTML = "";
+}

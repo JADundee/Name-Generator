3c98b467e291fed99d6b121a25919dfdcaad630b
JADundee <Jadundee@icloud.com>
2023-05-23 08:10:39 -0600

    Added displayNames() and sanitizeInput()

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const initApp = () => {
         clearSuggestions();
         const namesArray = generateNames();
         console.log(namesArray);
-        // Display names
+        displayNames(namesArray);
     });
 }
 
@@ -33,4 +33,29 @@ const generateNames = () => {
     const suggestion4 = nameOne[randomNumberArray[3]] + nameTwo[randomNumberArray[1]];
 
     return [suggestion1, suggestion2, suggestion3, suggestion4];
+}
+
+const displayNames = (namesArray) => {
+    const list = document.querySelector('.suggestionSection ol');
+    const rawFirstName = document.getElementById("submitSection__textInput").value;
+    const firstName = sanitizeInput(rawFirstName);
+    namesArray.forEach(name => {
+       /*  list.innerHTML += `<li>
+            <a href="https://worldofwarcraft.blizzard.com/en-us/character/${region}/${server}/${name}
+            </li>`; */
+            list.innerHTML += `<li>
+            <a href="https://yoututbe.com/${name}" target="_blank">${name}</a></li>`;
+            list.innerHTML += `<ul>
+            <li><a href="https://youtube.com/${firstName}s${name}" target="_blank">${firstName}s${name}</a></li>
+            <li><a href="https://youtube.com/${name}With${firstName}" target="_blank">${name}With${firstName}</a></li>
+            </ul>`;
+    });
+    const display = document.getElementById("suggestionSection");
+    if (display.classList.contains("hidden")) display.classList.toggle("hidden");
+}
+
+const sanitizeInput = (inputValue) => {
+    const div = document.createElement('div');
+    div.textContent = inputValue;
+    return div.innerHTML;
 }

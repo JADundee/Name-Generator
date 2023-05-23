41bfb50c9915fab6529bcb41dc753c6f84e351ed
JADundee <Jadundee@icloud.com>
2023-05-23 07:30:42 -0600

    Added generateNames function

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const initApp = () => {
     document.getElementById("submitForm").addEventListener("submit", (event) => {
         event.preventDefault();
         clearSuggestions();
-        // Generate names
+        const namesArray = generateNames();
+        console.log(namesArray);
         // Display names
     });
 }
@@ -16,4 +17,20 @@ const clearSuggestions = () => {
     if (!display.classList.contains("hidden")) display.classList.toggle("hidden");
     const list = document.querySelector('.suggestionSection ol');
     list.innerHTML = "";
+}
+
+const generateNames = () => {
+    const randomNumberArray = [];
+    for (let i = 0; i < 4;) {
+        const randomNumber = Math.floor(Math.random() * 10);
+        if (randomNumberArray.includes(randomNumber)) continue;
+        randomNumberArray.push(randomNumber);
+        i++;
+    }
+    const suggestion1 = nameOne[randomNumberArray[0]] + nameTwo[randomNumberArray[3]];
+    const suggestion2 = nameOne[randomNumberArray[1]] + nameTwo[randomNumberArray[0]];
+    const suggestion3 = nameOne[randomNumberArray[2]] + nameTwo[randomNumberArray[2]];
+    const suggestion4 = nameOne[randomNumberArray[3]] + nameTwo[randomNumberArray[1]];
+
+    return [suggestion1, suggestion2, suggestion3, suggestion4];
 }

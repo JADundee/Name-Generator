26f60daf4d923006a3d789349bf4808ecd6ebb40
JADundee <Jadundee@icloud.com>
2023-07-20 05:54:45 -0600

    Added A

--- js/worgen.js
+++ js/worgen.js
@@ -1,14 +1,25 @@
 export const worgenNameOneM = {
-    : "",
-    : "",
+    : "Looper",
+    : "Anatoly",
+    : "Jacob",
+    : "Kadus",
+    : "Falcon",
+    : "Josiah",
 }
 
 export const worgenNameOneF = {
-    : "",
-    : "",
+    : "Abigail",
+    : "Adella",
+    : "Mina",
+    : "Jeanine",
 }
 
 export const worgenNameTwo = {
-    : "",
-    : "",
+    : "Aldan",
+    : "Allen",
+    : "Amrell",
+    : "Anders",
+    : "Arkadian",
+    : "Atherton",
+    : "Avery",
 }

6d97cc3fab1968a874f55e5f8a4137596afcf28f
JADundee <Jadundee@icloud.com>
2023-07-21 11:35:14 -0600

    Added A/B/C

--- js/voidElf.js
+++ js/voidElf.js
@@ -1,14 +1,17 @@
 export const voidElfNameOneM = {
-    : "",
-    : "",
+    : "Aevedos",
+    : "Atynar",
+    : "Celosel",
 }
 
 export const voidElfNameOneF = {
-    : "",
-    : "",
+    : "Savia",
+    : "Aowyn",
+    : "Crysta",
+    : "Crystalynn",
 }
 
 export const voidElfNameTwo = {
-    : "",
-    : "",
+    : "Anguossa",
+    : "Nightgiver",
 }

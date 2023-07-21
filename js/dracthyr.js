9055b2507fd72c32ea6ed5a9e94a474cb05437c0
JADundee <Jadundee@icloud.com>
2023-07-21 07:09:17 -0600

    Added A

--- js/dracthyr.js
+++ js/dracthyr.js
@@ -1,14 +1,19 @@
 export const dracthyrNameOneM = {
-    : "",
-    : "",
+    : "Abereth",
+    : "Algeth'ar",
+    : "Alsiaszar",
+    : "Talon",
+    : "Azurathel",
 }
 
 export const dracthyrNameOneF = {
-    : "",
-    : "",
+    : "Alanatheth",
+    : "Algeth'ar",
+    : "Alzinathel",
+    : "Andoris",
 }
 
 export const dracthyrNameTwo = {
-    : "",
-    : "",
+    : "Freshscale",
+    : "Arrosh",
 }

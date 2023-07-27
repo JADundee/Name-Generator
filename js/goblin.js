4eb4749dce1e6cc4e1bb9c18709fc6a314ffa97e
JADundee <Jadundee@icloud.com>
2023-07-27 08:42:16 -0600

    Added A

--- js/goblin.js
+++ js/goblin.js
@@ -1,20 +1,33 @@
 export const goblinNameOneM = {
     : "shyster",
     : "Wulmort",
-    : "",
-    : "",
+    : "Ace",
+    : "Akazamzarak",
+    : "Alcorn",
+    : "Merd",
+    : "Feenix",
+    : "Arko",
+    : "Maxx",
+    : "Axle",
+    : "Azzleby",
 }
 
 export const goblinNameOneF = {
     : "Macey",
     : "Pella",
-    : "",
-    : "",
+    : "Coggeye",
+    : "Aja",
+    : "Alley",
+    : "Arva",
+    : "Audi",
+    : "Azley",
 }
 
 export const goblinNameTwo = {
     : "Jingglepocket",
     : "Brassbush",
-    : "",
-    : "",
+    : "Aimbot",
+    : "Archfeld",
+    : "Arcshine",
+    : "Avalanche",
 }

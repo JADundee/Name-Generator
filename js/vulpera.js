3bbaba3b8fac666d4fff707e792e2e67623ff50a
JADundee <Jadundee@icloud.com>
2023-08-02 03:32:16 -0600

    Added A/B/C/D

--- js/vulpera.js
+++ js/vulpera.js
@@ -1,11 +1,12 @@
 export const vulperaNameOneM = {
-    : "",
-    : "",
+    : "Noko",
+    : "Amre",
+    : "Dust",
 }
 
 export const vulperaNameOneF = {
-    : "",
-    : "",
+    : "Aisha",
+    : "Ahna",
 }
 
 export const vulperaNameTwo = {

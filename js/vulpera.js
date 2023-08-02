fb62e1f61a1690e5d15fcd9a988002b1fef94481
JADundee <Jadundee@icloud.com>
2023-08-02 03:34:06 -0600

    Added E/F/G

--- js/vulpera.js
+++ js/vulpera.js
@@ -2,14 +2,23 @@ export const vulperaNameOneM = {
     : "Noko",
     : "Amre",
     : "Dust",
+    : "Boots",
 }
 
 export const vulperaNameOneF = {
     : "Aisha",
     : "Ahna",
+    : "Eudora",
+    : "Eliani",
+    : "Evie",
+    : "Boots",
+    : "Fari",
+    : "Fifi",
+    : "Elly",
+    : "Gitah",
 }
 
 export const vulperaNameTwo = {
-    : "",
+    : "Fraze",
     : "",
 }

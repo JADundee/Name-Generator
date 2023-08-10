4dcf80b251d643777c68eb0e5c63cfee5c3a2f30
JADundee <Jadundee@icloud.com>
2023-08-10 03:03:32 -0600

    Added ghosts

--- js/zandalariTroll.js
+++ js/zandalariTroll.js
@@ -1,14 +1,38 @@
 export const zandalariTrollNameOneM = {
-    : "",
-    : "",
+    : "Garajal",
+    : "Geedae",
+    : "Jahn",
+    : "Manzul",
+    : "Zakrajan",
+    : "Ouda",
+    : "Ujunta",
+    : "Valjabu",
+    : "Venoxis",
+    : "Vilnakdor",
+    : "Wanbe",
+    : "Wunto",
+    : "Zanza",
+    : "Kanzim",
+    : "Zulsan",
 }
 
 export const zandalariTrollNameOneF = {
-    : "",
-    : "",
+    : "Dafini",
+    : "Shinga",
+    : "Duba",
+    : "Grenja",
+    : "Keula",
+    : "Malaja",
+    : "Ruka",
+    : "Salazae",
+    : "Tizeja",
+    : "Volni",
+    : "Zamcha",
+    : "Karaja",
+    : "Tastoa",
 }
 
 export const zandalariTrollNameTwo = {
-    : "",
-    : "",
+    : "Deathwalker",
+    : "Zubo",
 }
